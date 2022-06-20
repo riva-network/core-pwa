b12716216c538eec8d16f1a0b01ec28a12340dc0
baymax <david.lee4015@gmail.com>
2022-06-20 09:43:53 -0400

    fix: form field registration error

--- components/views/auth/LoginForm.tsx
+++ components/views/auth/LoginForm.tsx
@@ -26,7 +26,7 @@ export const LoginForm = () => {
       {({ register, formState }) => (
         <>
           <InputField type="text" prefixIcon={<UserIcon className="w-5 h-5 fill-gray-400" />} registration={register('username')} error={formState.errors['username']} placeholder="Username" className='w-full' />
-          <InputField type="password" prefixIcon={<LockClosedIcon className="w-5 h-5 fill-gray-400" />} registration={register('username')} error={formState.errors['password']} placeholder="Password" className='w-full' />
+          <InputField type="password" prefixIcon={<LockClosedIcon className="w-5 h-5 fill-gray-400" />} registration={register('password')} error={formState.errors['password']} placeholder="Password" className='w-full' />
           <div className='flex justify-between'>
             <Checkbox id="remember-me" label='Remember me' registration={register('rememberMe')} labelClass='text-xs' />
             <Link href='/forgot-password'><span className='text-link text-white text-sm cursor-pointer underline'>I forgot my password</span></Link>
@@ -34,6 +34,12 @@ export const LoginForm = () => {
           <Button type="submit" className='w-full'>
             Login
           </Button>
+          <div className='flex justify-center flex-col items-center'>
+            <p className="text-gray-400 text-sm">
+              Hey, don&apos;t you have an account yet? Do not worry!</p>
+            <Link href='/auth/login'>
+              <span className="text-white underline text-sm mt-2">Click and Create</span></Link>
+          </div>
         </>
       )}
     </Form>
